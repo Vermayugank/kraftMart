2160c00fca25095261c65a5cf7c70a5eb01dd771
Vermayugank <yugankv.ug20.ee@nitp.ac.in>
2023-07-05 10:37:42 +0530

    home added

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,19 @@
 import Image from 'next/image'
 import NavBar from '@/components/NavBar'
+import Hero from '@/components/Hero'
 
 
 export const metadata = {
   title: 'Kraftmart',
-  description: 'made By Yugank & Avinash',
+  description: 'Made By Yugank & Avinash',
 }
 
 export default function Home() {
   return (
+    <>
     <NavBar/>
+    <Hero/>
+    </>
+    
   )
 }
---
+++ src/components/Hero.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Hero() {
+    return (
+        <div className='bg-no-repeat bg-[url("https://png.pngtree.com/background/20220714/original/pngtree-black-abstract-background-picture-image_1605536.jpg")] bg-cover justify-center flex items-center pt-[9rem] lg:gap-1 md:gap-2 gap-6 flex-col text-5xl font-bold'>
+            <h1 data-aos="zoom-in" className='font-extrabold text-center lg:text-7xl md:text-6xl text-5xl lg:mx-8 md:mx-8 mx-4 text-white lg:mb-5 md:mb-5'>KraftMart</h1>
+            <h1 data-aos="zoom-in" className='font-bold text-center text-transparent lg:text-5xl md:text-4xl text-2xl mx-5 bg-clip-text bg-gradient-to-b from-gray-300 to-gray-600 lg:my-3 md:my-3'>NFT || MarketPlace || Buy && Sell</h1>
+            <h1 data-aos="zoom-in" className='font-extrabold text-center lg:text-7xl md:text-5xl text-4xl bg-clip-text text-transparent bg-gradient-to-r from-[#FE0000] to-[#FFB800]'>At one place</h1>
+        
+        </div>
+    )
+}
